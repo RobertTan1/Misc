49afc5212b6338028318c6beb285a6695e908fd2
Robert <ziomas1@hotmail.com>
2018-05-13 20:59:32 -0400

    Simplied code, added comments

--- riddle algo.R
+++ riddle algo.R
@@ -1,15 +1,21 @@
 # Define custom balls and directions
-input.vector <- c("L", "L", "L", "L")
+input.vector <- c("R", "R", "L")
 
 # Or generate a random set of balls, set to 15 balls currently
 input.vector <- sample(c("L","R"), 15, replace = T)
 
 # The algorithm
-{ 
-  count.vector <- 0
+{
+  # Initialize var for counting ghost-throughs
+  count <- 0
+  # While loop, continue so long as there are "R"s in the vector
   while ("R" %in% input.vector) {
-    count.vector <- count.vector + sum(which(input.vector == "R")[1] < which(input.vector == "L"))
+    # Evaluate boolean whether a "L" ball is right of the first "R" ball, then...
+    # Sum the number of TRUEs, add to count
+    count <- count + sum(which(input.vector == "R")[1] < which(input.vector == "L"))
+    # Remove the "R" ball that we just evaluated
     input.vector <- input.vector[-which(input.vector == "R")[1]]
   }
-  cat(paste(count.vector, "collisions until max entropy"))
+  # When loop ends, print out the result
+  cat(paste(count, "collisions until max entropy"))
 }
